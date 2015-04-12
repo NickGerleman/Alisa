673992013dc3a650334e3c23f19d586cdcc5ffe0
zachlones <zachlones@gmail.com>
2015-04-11 20:06:18 -0500

    response generation

--- src/main/java/tinder/CleverbotProfile.java
+++ src/main/java/tinder/CleverbotProfile.java
@@ -1,11 +1,18 @@
 package tinder;
 
+import chatterbot.ChatterBot;
+import chatterbot.ChatterBotFactory;
+import chatterbot.ChatterBotSession;
+import chatterbot.ChatterBotType;
+
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.TimeZone;
 
 public class CleverbotProfile extends Profile {
+	public HashMap<String, ChatterBot> chatterbots;
 
 	public CleverbotProfile(String name, String facebookToken){
 		super.profileName = name;
@@ -15,5 +22,30 @@ public class CleverbotProfile extends Profile {
 		DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm'Z'");
 		df.setTimeZone(tz);
 		timestamp = df.format(new Date());
+		chatterbots = new HashMap<String,ChatterBot>();
+	}
+
+	/*
+	Sends a response to the user maintaining a session with cleverbot
+	 */
+	public String sendResponse(String userId, String theirMessage){
+		String response = "";
+		if (chatterbots.containsKey(userId)){
+			ChatterBotFactory factory = new ChatterBotFactory();
+			try {
+				chatterbots.put(userId, factory.create(ChatterBotType.CLEVERBOT));
+			}catch (Exception e){
+				e.printStackTrace();
+			}
+		}
+		try {
+			ChatterBot bot = chatterbots.get(userId);
+			ChatterBotSession botsession = bot.createSession();
+			response = botsession.think(theirMessage);
+			super.sendMessage(userId,response);
+		}catch (Exception e){
+			e.printStackTrace();
+		}
+		return response;
 	}
 }
