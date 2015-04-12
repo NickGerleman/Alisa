87a911fbc5445fee3396936b894288daa409b43a
zachlones <zachlones@gmail.com>
2015-04-11 20:14:48 -0500

    Randomized Hodor!

--- src/main/java/tinder/HodorProfile.java
+++ src/main/java/tinder/HodorProfile.java
@@ -1,5 +1,7 @@
 package tinder;
 
+import java.util.Random;
+
 /**
  * Created by Zach on 4/11/2015.
  */
@@ -7,7 +9,19 @@ public class HodorProfile extends Profile{
 
     @Override
     public String sendResponse(String userId, String theirMessage) {
-        sendMessage(userId,"Hodor.");
-        return "Hodor";
+
+        //note a single Random object is reused here
+        Random randomGenerator = new Random();
+        int numHodor = randomGenerator.nextInt(20);
+        StringBuilder sb = new StringBuilder();
+        sb.append("Hodor");
+        for(int i = 0;i<numHodor;i++){
+            sb.append(" Hodor");
+            if(sb.length()%7==3)
+                sb.append(".");
+        }
+        sb.append(".");
+        sendMessage(userId,sb.toString());
+        return sb.toString();
     }
 }
